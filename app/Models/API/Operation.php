18dcb924742ea44915e6069425a24ffa37654061
Nikita <fomin.nv@phystech.edu>
2024-06-25 23:57:37 +0300

    Operation model

---
+++ app/Models/API/Operation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\API;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Operation extends Model
+{
+    use HasFactory, SoftDeletes, HasUuids;
+}
