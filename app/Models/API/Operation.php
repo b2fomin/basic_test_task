d0e12839238629d01184f4e731cf15c5dc572aaa
Nikita <fomin.nv@phystech.edu>
2024-06-27 23:03:23 +0300

    filter template (https://www.youtube.com/watch?v=cL1eXKsnRJI&list=PLd2_Os8Cj3t8pnG4ubQemoqnTwf0VFEtU&index=31)

--- app/Models/API/Operation.php
+++ app/Models/API/Operation.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\API;
 
+use App\Models\Traits\Filterable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,7 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Operation extends Model
 {
-    use HasFactory, SoftDeletes, HasUuids;
+    use HasFactory, SoftDeletes, HasUuids, Filterable;
 
     public function sub_operations() {
         return $this->hasMany(SubOperation::class);
--- app/Models/API/SubOperation.php
+++ app/Models/API/SubOperation.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\API;
 
+use App\Models\Traits\Filterable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,7 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SubOperation extends Model
 {
-    use HasFactory, SoftDeletes, HasUuids;
+    use HasFactory, SoftDeletes, HasUuids, Filterable;
 
     public function operation() {
         return $this->belongsTo(Operation::class);
