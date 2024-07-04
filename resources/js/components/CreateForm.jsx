92c8152b98a5f12297a4b08faf6f270a26a84135
Nikita <fomin.nv@phystech.edu>
2024-07-04 13:33:46 +0300

    template of delete form

--- resources/js/components/CreateForm.jsx
+++ resources/js/components/CreateForm.jsx
@@ -1,143 +1,72 @@
 import * as React from 'react';
-import { FormControl, useFormControlContext } from '@mui/base/FormControl';
-import { Input, inputClasses } from '@mui/base/Input';
-import { styled } from '@mui/system';
-import clsx from 'clsx';
-import Sidebar from './SideBar';
-import Box from '@mui/material/Box';
-import { DrawerHeader } from './SideBar';
 import Button from '@mui/material/Button';
-import { useNavigate } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
 
-const handleSubmit = async (event, model) => {
-  event.preventDefault();
-  await axios.post(`${(new URL(window.location.href)).origin}/api/v1/${model}`, {name: event.target[0].value})
-  .then(res => { 
-    return res.data.data.success ? window.location.href =
-      `${(new URL(window.location.href)).origin}/${model}`
-      : Promise.reject(res)})
-};
 
-export default function CreateForm({model}) {
+export default function FormDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Box sx={{display: 'flex', width: 200}}>
-      <Sidebar model={model}/>
-      <Box component="form" onSubmit={(event) => handleSubmit(event, model)} sx={{ flexGrow: 2, p: 3 }}>
-        <DrawerHeader />
-        <FormControl defaultValue="" required>
-          <Label>Name</Label>
-          <StyledInput placeholder="Write your name here" />
-          <HelperText />
-        </FormControl>
-        <Button variant="contained" type="submit">Send</Button>
-      </Box>
-    </Box>
+    <React.Fragment>
+      <Tooltip title="Delete" onClick={handleClickOpen}>
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          component: 'form',
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            const email = formJson.email;
+            console.log(email);
+            handleClose();
+          },
+        }}
+      >
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We
+            will send updates occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="email"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
   );
 }
-
-const StyledInput = styled(Input)(
-  ({ theme }) => `
-
-  .${inputClasses.input} {
-    width: 320px;
-    font-family: 'IBM Plex Sans', sans-serif;
-    font-size: 0.875rem;
-    font-weight: 400;
-    line-height: 1.5;
-    padding: 8px 12px;
-    border-radius: 8px;
-    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-    background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-    border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-    box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
-
-    &:hover {
-      border-color: ${blue[400]};
-    }
-
-    &:focus {
-      outline: 0;
-      border-color: ${blue[400]};
-      box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[600] : blue[200]};
-    }
-  }
-`,
-);
-
-const Label = styled(({ children, className }) => {
-  const formControlContext = useFormControlContext();
-  const [dirty, setDirty] = React.useState(false);
-
-  React.useEffect(() => {
-    if (formControlContext?.filled) {
-      setDirty(true);
-    }
-  }, [formControlContext]);
-
-  if (formControlContext === undefined) {
-    return <p>{children}</p>;
-  }
-
-  const { error, required, filled } = formControlContext;
-  const showRequiredError = dirty && required && !filled;
-
-  return (
-    <p className={clsx(className, error || showRequiredError ? 'invalid' : '')}>
-      {children}
-      {required ? ' *' : ''}
-    </p>
-  );
-})`
-  font-family: 'IBM Plex Sans', sans-serif;
-  font-size: 0.875rem;
-  margin-bottom: 4px;
-
-  &.invalid {
-    color: red;
-  }
-`;
-
-const HelperText = styled((props) => {
-  const formControlContext = useFormControlContext();
-  const [dirty, setDirty] = React.useState(false);
-
-  React.useEffect(() => {
-    if (formControlContext?.filled) {
-      setDirty(true);
-    }
-  }, [formControlContext]);
-
-  if (formControlContext === undefined) {
-    return null;
-  }
-
-  const { required, filled } = formControlContext;
-  const showRequiredError = dirty && required && !filled;
-
-  return showRequiredError ? <p {...props}>This field is required.</p> : null;
-})`
-  font-family: 'IBM Plex Sans', sans-serif;
-  font-size: 0.875rem;
-`;
-
-const blue = {
-  100: '#DAECFF',
-  200: '#b6daff',
-  400: '#3399FF',
-  500: '#007FFF',
-  600: '#0072E5',
-  900: '#003A75',
-};
-
-const grey = {
-  50: '#F3F6F9',
-  100: '#E5EAF2',
-  200: '#DAE2ED',
-  300: '#C7D0DD',
-  400: '#B0B8C4',
-  500: '#9DA8B7',
-  600: '#6B7A90',
-  700: '#434D5B',
-  800: '#303740',
-  900: '#1C2025',
-};
